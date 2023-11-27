97e3bd3d32e2a20aec666e5e6e308bd8c18d4310
Madi Karsybekov <madikarsybekov@gmail.com>
2023-11-27 21:18:36 +0100

    isConstant check with NULL 2

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -15,7 +15,7 @@ namespace ClassProject {
     }
 
     bool Manager::isConstant(BDD_ID f) {
-        return nodes[f].id == NULL && nodes[f].id == NULL && nodes[f].id == NULL;
+        return nodes[f].high == NULL && nodes[f].low == NULL && nodes[f].top_var == NULL;
         // return nodes[f].id == nodes[f].high && nodes[f].id == nodes[f].low && nodes[f].id == nodes[f].top_var;
     }
 
